101783facfde12739a4a39afc1e4302aa744e165
taunks <taunks@gmail.com>
2016-04-09 20:56:14 -0400

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'include/mysqli-connect.php' ?>
-<?php include "include/header.html" ?>
-<main>
-    <h1>Canine Database</h1>
-    <?php include 'include/submit-form.php' ?>
-    <?php include 'include/dog-form.php' ?>
-    <?php include 'include/get-dogs.php' ?>
-</main>
-<?php include "include/footer.html" ?>
